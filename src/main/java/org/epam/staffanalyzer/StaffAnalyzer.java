c3ba0b9a45c7eff76578fb964c75c4d3ff0ac2c8
Almas_Doskozhin <almas_doskozhin@epam.com>
2024-03-11 19:37:15 +0100

    Deleted logback library, since only JDK and JUnit are allowed

--- src/main/java/org/epam/staffanalyzer/StaffAnalyzer.java
+++ src/main/java/org/epam/staffanalyzer/StaffAnalyzer.java
@@ -4,28 +4,24 @@ import org.epam.staffanalyzer.entity.Employee;
 import org.epam.staffanalyzer.exception.CSVFormatException;
 import org.epam.staffanalyzer.parser.CSVParser;
 import org.epam.staffanalyzer.processor.ManagerProcessor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
 
 public class StaffAnalyzer {
-    static final Logger logger = LoggerFactory.getLogger(StaffAnalyzer.class);
 
     public static void main(String[] args) {
         CSVParser parser = new CSVParser();
 
         try {
             List<Employee> employees = parser.readEmployeesFromFile("employees.csv");
-            logger.debug("Number of employees = {}", employees.size());
 
             ManagerProcessor managerProcessor = new ManagerProcessor(employees);
             managerProcessor.analyzeManagerSalary();
 
             printResult(managerProcessor);
         } catch (IOException | CSVFormatException exception) {
-            logger.error("CSV parsing failed:", exception);
+            exception.printStackTrace();
         }
     }
 
